4bbb7231ecee8135817c2a6e29b0fc5192cd6093
Cibele Yumi <cibele.cruz@bgmrodotec.com.br>
2019-04-10 17:41:27 -0300

    WIP - Create a simple tests on angular project using mocha

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,6 @@
-// Reference mocha-typescript's global definitions:
-// tslint:disable-next-line: no-reference
-/// <reference path="../node_modules/mocha-typescript/globals.d.ts" />
 import { suite, test, slow, timeout } from 'mocha-typescript';
+import 'source-map-support/register';
 
-@suite(timeout(3000), slow(1000))
-class Hello {
-  @test world() {
-  }
+@suite class Two {
+  @test method() { }
 }
